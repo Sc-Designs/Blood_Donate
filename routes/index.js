2355badcdf1da30f2a8127336ca28e040b0d2346
Suvam Chakraborti <nfgaming4523@gmail.com>
2024-05-16 12:46:45 +0530

    Third CheckPoint

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+router.get("/feed", function (req, res, next) {
+  res.send("Welcome feed!");
+});
+router.get("/blood", function (req, res, next) {
+  res.send("Welcome blood!");
+});
+router.get("/report", function (req, res, next) {
+  res.send("Welcome report!");
 });
 
 module.exports = router;
